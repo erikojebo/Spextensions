abec68d66a692a0f02486abfe7d2872e0fe9293a
Erik Ojebo <erik.ojebo@gmail.com>
2010-09-23 23:18:28 +0200

    Added specs for SignalState

---
+++ src/NUnit/FactAttribute.cs
@@ -0,0 +1,6 @@
+using NUnit.Framework;
+
+namespace Spextensions.NUnit
+{
+    public class FactAttribute : TestAttribute { }
+}
---
+++ src/RhinoMocks/SignalStateSpecs.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using Spextensions.NUnit;
+
+namespace Spextensions.RhinoMocks
+{
+    [TestFixture]
+    public class SignalStateSpecs
+    {
+        private SignalState _signals;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _signals = new SignalState();
+        }
+
+        [Fact]
+        [ExpectedException(typeof(AssertionException))]
+        public void Assert_without_prior_signal_throws_ArgumentException()
+        {
+            _signals.AssertSignal("signal that has not been signaled");
+        }
+
+        [Fact]
+        public void Assert_after_signal_does_not_throw_exception()
+        {
+            _signals.Signal("expected signal");
+            _signals.AssertSignal("expected signal");
+        }
+
+        [Fact]
+        public void Assert_for_given_signals_after_multiple_signals_does_not_throw_exception()
+        {
+            _signals.Signal("expected signal");
+            _signals.Signal("other expected signal");
+            _signals.AssertSignal("expected signal");
+            _signals.AssertSignal("other expected signal");
+        }
+
+        [Fact]
+        [ExpectedException(typeof(AssertionException))]
+        public void Assert_after_only_signal_with_different_name_throws_ArgumentException()
+        {
+            _signals.Signal("other signal");
+            _signals.AssertSignal("expected signal");
+        }
+    }
+}
